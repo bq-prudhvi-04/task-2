fefcdfad943ccfda4739dfefd12aa10c3aa7c842
prudhvi <prudhvi.reddy@betaque.com>
2021-12-01 10:56:58 +0530

    security group added

--- modules/SG/main.tf
+++ modules/SG/main.tf
@@ -49,29 +49,3 @@ resource "aws_security_group" "webserver_sg" {
 }
 
 
-resource "aws_security_group" "worker_sg" {
-  name        = "woker_sg"
-  vpc_id      = var.vpc_id
-
-  ingress {
-    
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    
-  }
-
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    
-  }
-
-  tags = {
-    Name = "worker_sg"
-  }
-}
